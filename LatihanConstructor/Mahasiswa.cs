026c2cfc606506a364f9685120218ee26a0e81b9
ratutriaarospurnama <ratutriaarospurnama19@gmail.com>
2019-04-02 12:17:07 +0700

    LatihanConstructor

---
+++ LatihanConstructor/Mahasiswa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanConstructor
+{
+    public class Mahasiswa
+    {
+        public Mahasiswa()
+        {
+        }
+
+
+        public Mahasiswa(string nim, string nama)
+        {
+            Nim = nim;
+            Nama = nama;
+        }
+
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+    }
+}
---
+++ LatihanConstructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1 = new Mahasiswa();
+            CetakHasil(mhs1);
+
+            Console.WriteLine();
+
+            Mahasiswa mhs2 = new Mahasiswa("2336", "Ratu");
+            CetakHasil(mhs2);
+
+            Console.ReadKey();
+        }
+
+        static void CetakHasil(Mahasiswa mhs)
+        {
+            Console.WriteLine("Nim: {0}", mhs.Nim);
+            Console.WriteLine("Nama: {0}", mhs.Nama);
+        }
+    }
+}
